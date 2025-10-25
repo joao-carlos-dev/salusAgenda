d0acd25fc8197256a71a66b828f87e8bb3225e2e
joao-carlos-dev <joaocarlosifsc@hotmail.com>
2025-10-25 19:15:20 -0300

    criando o componente Login

--- salusApp/src/App.tsx
+++ salusApp/src/App.tsx
@@ -1,12 +1,14 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './Global.css'
+import Login from './components/Login/Login';
 
 function App() {
   return (
-    <>
-      <div>
-        <h1>Salus Agenda</h1>
-      </div>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login/>} />
+      </Routes>
+    </Router>
   )
 }
 
---
+++ salusApp/src/components/Login/Login.tsx
@@ -0,0 +1,60 @@
+import { useState } from "react";
+import type { LoginData } from "../../interfaces/LoginData";
+import isError from "../../Utils/isError";
+import './Login.css';
+import Salustext from "../../img/sallustext.png";
+
+const Login = () => {
+    const [loginData, setLoginData] = useState<LoginData> ({
+        email: '',
+        password: ''
+    });
+
+    const handleLogin = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const {name, value} = event.target;
+        setLoginData({
+            ...loginData,
+            [name]: value
+        }); 
+    }
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        try{
+            console.log("Vêm api");
+        } catch(e: unknown) {
+            const erroMessage = isError(e) ? e.message : "An unknown error corrured";
+            console.error("Login error: ", erroMessage);
+        }
+    }
+  return (
+    <>
+      <section className="containerLogin">
+        <img src={Salustext} alt="Salus Agenda" />
+        <div className="loginTitleContainer">
+            <h1>Faça seu login</h1>
+        </div>
+        <div className="login">
+
+            <form className="loginForm" onSubmit={handleSubmit}>
+                <label htmlFor="text_mail" className="inputLabel">E-mail</label>
+                <input type="text" name="email" placeholder="Digite seu e-mail" value={loginData.email} onChange={handleLogin} id="text_mail" />
+                
+                <label htmlFor="password" className="inputLabel">Senha</label>
+                <input type="password" name="password" placeholder="Digite sua senha" value={loginData.password} onChange={handleLogin}/>
+
+                <button className="button-submit" type="submit">
+                    Login
+                </button>
+            </form>
+
+        </div>
+        <a href="#" className="registerText">
+            Ainda não tem uma conta? Registre-se
+        </a>
+      </section>
+    </>
+  );
+};
+
+export default Login;
